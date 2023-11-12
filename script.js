722b7bb54019554febad8e9b933d3d7dd9a6e5ce
tohoku8141 <150413148+tohoku8141@users.noreply.github.com>
2023-11-12 22:36:34 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function findPrimes() {
+    const userInput = document.getElementById('userInput').value;
+    const resultElement = document.getElementById('result');
+
+    const inputNumber = parseInt(userInput);
+    if (isNaN(inputNumber) || inputNumber < 2 || !Number.isInteger(inputNumber)) {
+        resultElement.innerHTML = '適切な整数を入力してください（2以上の整数）。';
+        return;
+    }
+
+    if (isPrime(inputNumber)) {
+        resultElement.innerHTML = `${inputNumber} は素数です。`;
+    } else {
+        let smallerPrime = inputNumber - 1;
+        while (!isPrime(smallerPrime) && smallerPrime > 1) {
+            smallerPrime--;
+        }
+
+        let largerPrime = inputNumber + 1;
+        while (!isPrime(largerPrime)) {
+            largerPrime++;
+        }
+
+        resultElement.innerHTML = `${inputNumber} は素数ではありません。<br>(${smallerPrime}) と (${largerPrime}) の間の合成数です。`;
+    }
+}
